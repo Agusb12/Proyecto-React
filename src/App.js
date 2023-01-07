a186cbb2656eef827a1619e1140b47d03b026b90
AgustinBolla <agustinbolla@gmail.com>
2023-01-07 11:26:06 -0300

    Nav-Bar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import { FaBeer } from "react-icons/fa";
+function App() {
+  return (
+    <div className="App">
+     <h1 className="react">Titulo</h1>
+     <NavBar />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import './NavBar.css';
+
+function  NavBar () {
+    return(
+        <div className="div-nav">
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
