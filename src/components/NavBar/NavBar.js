eeeccd1fe639a6e772cf112463d2679d8a754252
AgustinBolla <agustinbolla@gmail.com>
2023-01-09 09:45:03 -0300

    first challenge

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,18 @@
 import './NavBar.css';
-
+import { FaAlignJustify } from "react-icons/fa";
+import React, { ReactDOM } from 'react';
+ 
 function  NavBar () {
     return(
-        <div className="div-nav">
-            <ul>
-                <li></li>
+        <nav className="div-nav">
+                <ul>
+            <h1>PHONE FIXERS</h1>
+                    <li>Inicio</li>
+                    <li>Formulario</li>
+                    <li>Contacto</li>
+                
             </ul>
-        </div>
+        </nav>
     )
 }
 
